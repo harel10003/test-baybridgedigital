e5747e8f9a17493107827cd50613f6df30899bbf
harel10003 <harel10003@gmail.com>
2022-05-10 11:49:25 +0300

    css

--- src/components/Haeder.jsx
+++ src/components/Haeder.jsx
@@ -8,30 +8,37 @@ import Cart from './Cart';
 function haeder({ totolPrice, list, setShow, show, delivery }) {
 	return (
 		<div className="haeder_container">
-			<div className="haeder_icon" onClick={() => setShow(false)}>
-				🏠
+			<div>
+				<div className="haeder_icon" onClick={() => setShow(false)}>
+					🏠
+				</div>
+
+				<div>API- SHOP</div>
 			</div>
 
-			<div>API- SHOP</div>
-			<div className="haeder_icon" onClick={() => setShow(true)}>
-				🛒 {totolPrice.toFixed(2)}$
-			</div>
-			<div className="haeder_icon" onClick={() => setShow(false)}>
-				<MdDeliveryDining /> {delivery}
-			</div>
-			<div
-				style={{
-					display:
-						list.length == 0 && show == false ? 'none' : 'inline',
-				}}
-			>
-				<button onClick={() => setShow(!show)}>
-					{show ? 'close cart' : 'open cart'}
-				</button>
-			</div>
-			<div style={{ display: show ? 'inline' : 'none' }}>
-				<Cart />
-			</div>
+			<divs>
+				<div className="haeder_icon" onClick={() => setShow(true)}>
+					🛒 {totolPrice.toFixed(2)}$
+				</div>
+				<div className="haeder_icon" onClick={() => setShow(false)}>
+					<MdDeliveryDining /> {delivery}
+				</div>
+				<div
+					style={{
+						display:
+							list.length == 0 && show == false
+								? 'none'
+								: 'inline',
+					}}
+				>
+					<button onClick={() => setShow(!show)}>
+						{show ? 'close cart' : 'open cart'}
+					</button>
+				</div>
+				<div style={{ display: show ? 'inline' : 'none' }}>
+					<Cart />
+				</div>
+			</divs>
 		</div>
 	);
 }
